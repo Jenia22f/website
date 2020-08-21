5d8e6bfabf9aa17f434bcbc0c82bcb47c50bf8ad
Jenia22f <Nofightnoglory22>
2020-08-21 14:29:47 +0300

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,85 @@
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'universal',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: "viewport", content:"width=device-width, initial-scale=1.0"}
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: "stylesheet",
+        href: "https://use.fontawesome.com/releases/v5.14.0/css/all.css",
+        integrit: "sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc",
+        crossorigin: "anonymous"
+      },
+      {
+        rel:"stylesheet",
+        href:"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"},
+      { rel:"stylesheet", href:"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"},
+    ],
+    script: [
+      { src:"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"},
+      { src:"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"},
+      { src: 'https://unpkg.com/jquery@3.2.1/dist/jquery.min.js' },
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/node_modules/bootstrap/dist/css/bootstrap.min.css'],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  build: {
+    /**
+     * add external plugins
+     */
+    vendor: [],
+    plugins: [
+    ],
+    /*
+    ** Run ESLint on save
+    */
+  }
+}
