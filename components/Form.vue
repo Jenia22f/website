94eac5267417bddc07b184ad0e293e8dcd56bb78
Jenia22f <Nofightnoglory22>
2020-09-16 16:13:20 +0300

    all changes

--- components/Form.vue
+++ components/Form.vue
@@ -423,7 +423,8 @@
   }
 
   .form-registration_form-number-today_num:not(:last-child) {
-    margin-right: 10px;
+    margin-right: 5px;
+    margin-left: 5px;
   }
 
   .form-registration_form-number-today_p {
